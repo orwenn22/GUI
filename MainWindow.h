39d11d339c5d159e809b3886ee6c43a92474cd23
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2022-12-20 16:12:15 +0100

    Somewhat get rid of weird global variables. (but replace them with ifdefs and keep ResizeMainWindow)

--- MainWindow.h
+++ MainWindow.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_WINDOW_H
 #define MAIN_WINDOW_H
 
-extern int g_maxfps;
+//TODO : get rid of this at some point.
+#define g_winwidth GetRenderWidth()
+#define g_winheight GetRenderHeight()
 
-extern int g_winwidth;
-extern int g_winheight;
-
-void InitMainWindow(int width, int height, const char* title);
 void ResizeMainWindow(int newwidth, int newheight);
-void SetMainFPS(int fps);
 
 #endif
